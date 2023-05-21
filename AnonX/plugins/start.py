c00a37a110f0be1aa9203844c10704bda5a90791
Gumnamsingle <130532799+Gumnamsingle@users.noreply.github.com>
2023-05-21 08:44:39 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CXpu52QACQfBkaYKBFfAmplIVyPoETix1vp_s6QAC-gEAArN76FZySZDaQfBt4h4E")
+            await message.reply_sticker("CAACAgUAAx0Ca1Y2WQABBWBkZGmI0qq6kifK5oZIQrX5AAEGPCfTAALFBwACYPdRV1gRTyUBrdODHgQ")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAx0CXpu52QACQfBkaYKBFfAmplIVyPoETix1vp_s6QAC-gEAArN76FZySZDaQfBt4h4E")
+                await message.reply_sticker("CAACAgUAAx0Ca1Y2WQABBWBkZGmI0qq6kifK5oZIQrX5AAEGPCfTAALFBwACYPdRV1gRTyUBrdODHgQ")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
